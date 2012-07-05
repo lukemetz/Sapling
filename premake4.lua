16622e05942fe0e41e130bc908e5628ac3731862
lukemetz <luke.metz@students.olin.edu>
2012-07-04 21:33:21 -0400

    cleaned up building process

--- premake4.lua
+++ premake4.lua
@@ -1,9 +1,10 @@
 solution "Sapling"
    configurations { "Debug", "Release" }
- 
+   postbuildcommands {"LD_LIBRARY_PATH=."} 
    project "Sapling"
-      kind "ConsoleApp"
+      kind "SharedLib"
       language "C++"
+      targetdir 'lib' 
       files { "*.hpp", "*.cpp" , "*.h", "Systems/*", "Components/*", "Wrappers/*.h","Wrappers/*.cpp"}
       links{'glfw','Horde3D', 'Horde3DUtils', "jansson"}
       includedirs{"."} 
@@ -11,10 +12,49 @@ solution "Sapling"
         libdirs{'/usr/local/lib'}
         includedirs{'/usr/local/include/'}
       end
-      
+      buildoptions{"-Wall"} 
       configuration "Debug"
          defines { "DEBUG" }
          flags { "Symbols" }
       configuration "Release"
          defines { "NDEBUG" }
-         flags { "Optimize" }    
+         flags { "Optimize" }
+
+  project "Samples"
+    kind "ConsoleApp"
+    language "C++"
+      files { "Samples/*.h", "Samples/*.cpp"}
+      links{"Sapling", 'glfw','Horde3D', 'Horde3DUtils', "jansson"}
+      includedirs{"."} 
+      if os.is('linux') then --Try to find horde3d and glfw
+        libdirs{'/usr/local/lib'}
+        includedirs{'/usr/local/include/'}
+      end
+      buildoptions{"-Wall"} 
+      targetdir "Samples"
+      configuration "Debug"
+         defines { "DEBUG" }
+         flags { "Symbols" }
+      configuration "Release"
+         defines { "NDEBUG" }
+         flags { "Optimize" }
+
+
+  project "Tests"
+    kind "ConsoleApp"
+    language "C++"
+      files { "Tests/*.h", "Tests/*.cpp"}
+      links{"Sapling", 'glfw','Horde3D', 'Horde3DUtils', "jansson", "gtest", "stdc++"}
+      includedirs{"."} 
+      if os.is('linux') then --Try to find horde3d and glfw
+        libdirs{'/usr/local/lib'}
+        includedirs{'/usr/local/include/'}
+      end
+      buildoptions{"-Wall"} 
+      targetdir "Tests"
+      configuration "Debug"
+         defines { "DEBUG" }
+         flags { "Symbols" }
+      configuration "Release"
+         defines { "NDEBUG" }
+         flags { "Optimize" }
