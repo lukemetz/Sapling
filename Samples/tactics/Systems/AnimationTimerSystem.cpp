b202c203901bb2b373c0b18914fdbc95e51be8fa
lukemetz <luke.metz@students.olin.edu>
2012-07-14 12:31:14 -0400

    Convert to range based for loops

--- Samples/tactics/Systems/AnimationTimerSystem.cpp
+++ Samples/tactics/Systems/AnimationTimerSystem.cpp
@@ -10,10 +10,8 @@ void AnimationTimerSystem::run(float dt)
   std::vector<Entity*> entities;
   ensys->getEntities<AnimationTimerComponent>(entities);
 
-  std::vector<Entity*>::iterator it;
-  for(it = entities.begin(); it != entities.end(); ++it) {
-    Entity *en = *it;
-    en->getAs<AnimationTimerComponent>()->time += dt;
+  for(Entity *entity : entities) {
+    entity->getAs<AnimationTimerComponent>()->time += dt;
   }
 }
 
---
+++ Samples/tactics/Systems/MovementSelectorSystem.cpp
@@ -0,0 +1,47 @@
+#include "MovementSelectorSystem.h"
+
+#include "Components/MovementComponent.h"
+#include "Components/AnimationTimerComponent.h"
+#include <Components/components.h>
+#include <App.h>
+#include "Utils.h"
+
+MovementSelectorSystem::MovementSelectorSystem()
+{
+  ensys = EntitySystem::sharedInstance();
+}
+
+void MovementSelectorSystem::run(float dt)
+{
+  std::vector<Entity*> entities;
+  ensys->getEntities<MovementComponent>(entities);
+  std::vector<Entity*>::iterator it;
+  Application *app = Application::sharedInstance();
+
+  std::vector<Entity*> cameraEntities;
+  ensys->getEntities<CameraComponent>(cameraEntities);
+
+  Entity* cameraEntity = cameraEntities[0];
+
+  for(Entity *entity : entities) {
+    MovementComponent *mc = entity->getAs<MovementComponent>();
+    TransformComponent *tc = entity->getAs<TransformComponent>();
+    InputComponent *ic = entity->getAs<InputComponent>();
+
+    if (mc != nullptr && ic != nullptr && tc != nullptr) {
+      if (ic->mouseButtons[0] == true && ic->prevMouseButtons[0] == false)
+      {
+        float normalizedMouseX = ic->mouseX/app->appWidth;
+
+        float normalizedMouseY = (app->appHeight - ic->mouseY)/app->appHeight;
+        float ox, oy, oz, dx, dy, dz;
+
+        H3DNode node = h3dutPickNode(cameraEntity->getAs<CameraComponent>()->node, normalizedMouseX, normalizedMouseY);
+        Entity * en = Utils::sharedInstance()->getEntityForNode(node);
+        if (en != nullptr) {
+          en->getAs<TransformComponent>()->scale.x *= 2;
+        }
+      }
+    }
+  }
+}
