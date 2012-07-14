b8ac5aa0ad277671433d13dc6ec108bf439582b5
lukemetz <luke.metz@students.olin.edu>
2012-07-14 11:09:46 -0400

    Fixed relating h3d child nodes to Entities.

--- Utils.cpp
+++ Utils.cpp
@@ -18,6 +18,9 @@ Utils *Utils::sharedInstance()
 void Utils::addNodeEntity(H3DNode node, Entity *entity)
 {
   nodeToEntity[node] = entity;
+  for (int i=0; 0 != h3dGetNodeChild(node, i); i++){
+    addNodeEntity(h3dGetNodeChild(node, i), entity);
+  }
 }
 
 Entity *Utils::getEntityForNode(H3DNode node)
