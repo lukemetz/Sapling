2b20550b86ded09fcedab0e96177aab8c57d278d
lukemetz <luke.metz@students.olin.edu>
2012-08-04 17:04:44 -0400

    Added health and resists

--- Samples/tactics/Components/UnitComponent.cpp
+++ Samples/tactics/Components/UnitComponent.cpp
@@ -1,5 +1,14 @@
 #include "UnitComponent.h"
 
-UnitComponent::UnitComponent() {
-  
+UnitComponent::UnitComponent()
+{
+  health = 200;
+  resists.resize(2, 0);
+  resists[kResistArmor] = 10.f;
+  resists[kResistMagicResist] = 5.0f;
+}
+
+void UnitComponent::takeDamage(Resist type, float amount)
+{
+  health = amount/(1+resists[type]/100.f);
 }
--- Samples/tactics/Components/UnitComponent.h
+++ Samples/tactics/Components/UnitComponent.h
@@ -1,7 +1,18 @@
+#pragma once
 #include <EntitySystem.h>
+
+enum Resist
+{
+  kResistArmor,
+  kResistMagicResist
+};
 
 struct UnitComponent : public Component
 {
+  int health;
+  std::vector<float> resists;
 
+  void takeDamage(Resist type, float amount);
   UnitComponent();
+
 };
